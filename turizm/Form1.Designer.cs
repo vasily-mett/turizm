ce69225784bb934a2f73dc5ebfc09810fc5a2acf
vasich <vass.vass@mail.ru>
2018-04-09 19:11:18 +0300

    first

--- turizm/Form1.Designer.cs
+++ turizm/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonUpdateDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonUpdateDB
+            // 
+            this.buttonUpdateDB.Location = new System.Drawing.Point(227, 12);
+            this.buttonUpdateDB.Name = "buttonUpdateDB";
+            this.buttonUpdateDB.Size = new System.Drawing.Size(91, 23);
+            this.buttonUpdateDB.TabIndex = 0;
+            this.buttonUpdateDB.Text = "Обновить БД";
+            this.buttonUpdateDB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(459, 432);
+            this.Controls.Add(this.buttonUpdateDB);
+            this.Name = "Form1";
+            this.Text = "Туризм";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonUpdateDB;
     }
 }
 
